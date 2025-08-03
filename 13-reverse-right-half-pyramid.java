1cf821441cb24f1ce3d955c56a23ac83df74c70d
Akhil Anand Balu <147290597+akhilanand13@users.noreply.github.com>
2025-08-03 12:28:25 +0530

    Add files via upload

---
+++ 13-reverse-right-half-pyramid.java
@@ -0,0 +1,14 @@
+//https://www.geeksforgeeks.org/java/java-pattern-programs/
+//Reverse Right Half Pyramid (Pattern 13)
+//The outer loop controls the number of rows and the inner loop prints the actual pattern
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        for(int i = 0; i < 5; i++){
+            for(int j = i; j < 5; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
