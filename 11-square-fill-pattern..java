710623bd2e3abbbcd74ee09f3a7f994dc2ac2981
Akhil Anand Balu <147290597+akhilanand13@users.noreply.github.com>
2025-08-04 18:12:27 +0530

    Add files via upload

---
+++ 11-square-fill-pattern..java
@@ -0,0 +1,11 @@
+//https://www.geeksforgeeks.org/java/java-pattern-programs/
+//Square Fill Pattern (Pattern 11)
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        for(int i = 0; i < 6; i++){
+            System.out.print("* ".repeat(6));
+            System.out.println();
+        }
+    }
+}
